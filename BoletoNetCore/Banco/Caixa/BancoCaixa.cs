1e6775b0118cebf6771cdf9cb0eba674803cb6df
João Bernardo <joaob404@gmail.com>
2024-10-28 21:53:17 -0300

    000 - Ajuste homolação de boletos caixa

--- BoletoNetCore/Banco/Caixa/BancoCaixa.cs
+++ BoletoNetCore/Banco/Caixa/BancoCaixa.cs
@@ -35,7 +35,7 @@ namespace BoletoNetCore
                 if (String.IsNullOrEmpty(Beneficiario.CodigoDV))
                     throw new Exception($"Dígito do código do beneficiário ({codigoBeneficiario}) não foi informado.");
 
-                Beneficiario.CodigoFormatado = $"{contaBancaria.Agencia} / {codigoBeneficiario}-{Beneficiario.CodigoDV}";
+                Beneficiario.CodigoFormatado = $"{contaBancaria.Agencia} / {codigoBeneficiario.PadLeft(7, '0')}-{Beneficiario.CodigoDV}";
             }
             else if (codigoBeneficiario.Length == 7)
             {
